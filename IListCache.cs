f2d833ecae26007d7dfa3da7c570fea19a2d5d74
Jubotech <boris.mct@gmail.com>
2014-10-08 17:08:10 -0600

    Cleanup

--- IListCache.cs
+++ IListCache.cs
@@ -3,19 +3,19 @@ using System.Collections.Generic;
 
 namespace Jubotech.Caching
 {
-    public interface IListCache
-    {
+	public interface IListCache
+	{
 		List<TDto> GetListOf<TEntity, TDto>(Func<TEntity, bool> predicate = null, string additionalKey = "") 
 			where TEntity : class 
 			where TDto : class, new();
 
 		List<TDto> GetNewListOf<TEntity, TDto>(Func<TEntity, bool> predicate = null, string additionalKey = "")
-            where TEntity : class
-            where TDto : class, new();
+			where TEntity : class
+			where TDto : class, new();
         
 		List<TEntity> GetListOf<TEntity>(Func<TEntity, bool> predicate = null, string additionalKey = "")
-            where TEntity : class;
+			where TEntity : class;
 
 		void ClearListCache();
-    }
+	}
 }
